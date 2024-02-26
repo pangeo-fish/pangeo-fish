fe13ac06f235da7825ae1bc1913617216d824e15
Justus Magin <keewis@posteo.de>
2024-02-26 12:09:51 +0100

    allow scaling the cluster immediately after creation
    This is necessary for `dask-jobqueue` clusters, as those will only
    create workers by submitting jobs to the jobqueue after scaling.

--- pangeo_fish/cluster.py
+++ pangeo_fish/cluster.py
@@ -4,7 +4,7 @@ scheduler_address_re = re.compile(r"tcp://\[?[0-9a-f:.]+\]?:[0-9]+")
 scheme_re = re.compile(r"(?P<type>[^:]+):(?P<data>.+)")
 
 
-def create_cluster(spec, **kwargs):
+def create_cluster(spec, *, scale=None, **kwargs):
     """create a cluster from a spec and return a client
 
     Parameters
@@ -16,6 +16,8 @@ def create_cluster(spec, **kwargs):
         - the scheduler address of a running cluster: `tcp://<ip>:<port>`
         - `dask-jobqueue:<path-to-spec-file>`
         - `dask-hpcconfig:<name>`
+    scale : int or mapping, optional
+        Scale the cluster after creation. Not allowed with scheduler addresses.
     **kwargs
         Additional keyword arguments passed to the cluster
     """
@@ -23,27 +25,36 @@ def create_cluster(spec, **kwargs):
         from distributed import LocalCluster
 
         cluster = LocalCluster(**kwargs)
-
-        return cluster.get_client()
     elif scheduler_address_re.match(spec) is not None:
         from distributed import Client
 
         client = Client(spec, **kwargs)
 
+        if scale is not None:
+            raise ValueError(
+                "it is impossible to modify the cluster using the scheduler address"
+            )
+
         return client
+    else:
+        match = scheme_re.match(spec)
+        if match is None:
+            raise ValueError(f"invalid scheme format: {spec!r}")
 
-    match = scheme_re.match(spec)
-    if match is None:
-        raise ValueError(f"invalid scheme format: {spec!r}")
+        groups = match.groupdict()
+        if groups["type"] == "dask-jobqueue":
+            raise NotImplementedError("does not work yet")
+        elif groups["type"] == "dask-hpcconfig":
+            import dask_hpcconfig
 
-    groups = match.groupdict()
-    if groups["type"] == "dask-jobqueue":
-        raise NotImplementedError("does not work yet")
-    elif groups["type"] == "dask-hpcconfig":
-        import dask_hpcconfig
+            cluster = dask_hpcconfig.cluster(groups["data"], **kwargs)
+        else:
+            raise ValueError("unknown cluster scheme")
 
-        cluster = dask_hpcconfig.cluster(groups["data"], **kwargs)
+    if scale is not None:
+        if isinstance(scale, dict):
+            cluster.scale(**scale)
+        else:
+            cluster.scale(scale)
 
-        return cluster.get_client()
-
-    raise ValueError("unknown cluster type")
+    return cluster.get_client()
