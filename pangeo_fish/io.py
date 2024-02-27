ebbfddb02294e9f2765fa2828c3532bfd7c3c7d4
Justus Magin <keewis@users.noreply.github.com>
2024-02-27 14:19:45 +0100

    allow missing receiver deployment data (#63)
    * allow missing receiver deployment data
    
    * allow zero-row csv files for `stations` and `acoustic`
    
    * explain what to do if the acoustic detections don't exist
    
    * describe the format of the `stations` file
    
    * use dummy data for the station example

--- pangeo_fish/io.py
+++ pangeo_fish/io.py
@@ -113,18 +113,20 @@ def open_tag(root, name, storage_options=None):
 
     metadata = json.load(mapper.dirfs.open(f"{name}/metadata.json"))
 
-    stations = pd.read_csv(
-        mapper.dirfs.open("stations.csv"),
-        parse_dates=["deploy_time", "recover_time"],
-        index_col="deployment_id",
-    ).pipe(tz_convert, {"deploy_time": None, "recover_time": None})
-
     mapping = {
         "/": xr.Dataset(attrs=metadata),
-        "stations": stations.to_xarray(),
         "dst": dst.to_xarray(),
         "tagging_events": tagging_events.to_xarray(),
     }
+
+    if mapper.dirfs.exists(f"{name}/stations.csv"):
+        stations = pd.read_csv(
+            mapper.dirfs.open("stations.csv"),
+            parse_dates=["deploy_time", "recover_time"],
+            index_col="deployment_id",
+        ).pipe(tz_convert, {"deploy_time": None, "recover_time": None})
+        if len(stations) > 0:
+            mapping["stations"] = stations.to_xarray()
 
     if mapper.dirfs.exists(f"{name}/acoustic.csv"):
         acoustic = pd.read_csv(
@@ -132,7 +134,8 @@ def open_tag(root, name, storage_options=None):
             parse_dates=["time"],
             index_col="time",
         ).tz_convert(None)
-        mapping["acoustic"] = acoustic.to_xarray()
+        if len(acoustic) > 0:
+            mapping["acoustic"] = acoustic.to_xarray()
 
     return datatree.DataTree.from_dict(mapping)
 
