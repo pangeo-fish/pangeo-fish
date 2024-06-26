b1f6dc38835703fe95c68b56a2d126d1e6ed7ce2
aderrien7 <alexandrederrien.sti2d@gmail.com>
2024-06-26 14:23:42 +0000

    files for dealing with powerplant warm water and notebook execution
    Description :
    - heat.py : routine used to generate and replace emission for the marked time stamps called in the 5th section of the pangeo-fish_pp template notebook.
    - data_conversion.py : python file storing multiple functions that help formatting raw files in pangeo-fish format.
    - data_formatting.ipynb : notebook that showcase how using the functions from heat.py with the final part that generates for all your files and stores them to the bucket.
    - pangeo-fish papermill.ipynb : former template notbeook that generates tracks and store them on the bucket.
    - pangeo-fish_PP.ipynb : template notbeook that generates tracks by taking into account the powerplant warm water detections and store them on the bucket.
    - papermill_launcher.ipynb : notebook that launch a given list of tags for processing.

---
+++ build/lib/pangeo_fish/heat.py
@@ -0,0 +1,35 @@
+from xarray_healpy.conversions import geographic_to_cartesian
+from pangeo_fish.healpy import (
+    astronomic_to_cartesian,
+    astronomic_to_cell_ids,
+    geographic_to_astronomic,
+)
+from xarray_healpy.operations import buffer_points
+import xarray as xr
+
+def powerpalnt_emission_map(pp_map,emission,buffer_size,rot):
+    grid = emission[["time", "cell_ids", "mask"]].compute()
+    cell_ids = grid["cell_ids"]
+    
+    positions = geographic_to_cartesian(
+        lon=pp_map["Longitude"],
+        lat=pp_map["Latitude"],
+        rot=rot,
+    )
+    
+    masks = buffer_points(
+        cell_ids,
+        positions,
+        buffer_size=buffer_size.m_as("m"),
+        nside=2 ** cell_ids.attrs["level"],
+        factor=2**16,
+        intersect=True,
+    ).drop_vars(["cell_ids"])
+    combined_masks = masks.sum(dim="index")
+    return combined_masks.transpose("y", "x").assign_attrs({"buffer_size": buffer_size.m_as("m")}).where(grid["mask"])
+
+def heat_regulation(emission,detections,combined_masks):
+    for time in emission["time"]:
+        if detections["predicted_label"].loc[time] == 1.0:
+             emission["pdf"].loc[time] = combined_masks
+    return emission
