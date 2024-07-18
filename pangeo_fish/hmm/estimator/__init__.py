da39b9fbf1b64ab968831dedaeddcbf7fcc807db
Justus Magin <keewis@posteo.de>
2024-07-18 11:45:39 +0200

    adjust the imports

--- pangeo_fish/hmm/estimator/__init__.py
+++ pangeo_fish/hmm/estimator/__init__.py
@@ -1,1 +1,2 @@
-from pangeo_fish.hmm.estimator.split import EagerEstimator  # noqa: F401
+from pangeo_fish.hmm.estimator.cached import CachedEstimator  # noqa: F401
+from pangeo_fish.hmm.estimator.eager import EagerEstimator  # noqa: F401
