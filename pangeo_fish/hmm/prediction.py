1bf804a2e6e9588eed9e114a2f1f98ec52483896
Justus Magin <keewis@posteo.de>
2024-08-23 14:10:52 +0200

    add a predictor using the `healpix-convolution` library

--- pangeo_fish/hmm/prediction.py
+++ pangeo_fish/hmm/prediction.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any
 
 import dask.array as da
+import healpix_convolution as hc
+import healpix_convolution.convolution
 import numpy as np
 import scipy.ndimage
+from xarray.namedarray._typing import _arrayfunction_or_api as _ArrayLike
+from xdggs.grid import DGGSInfo
 
 
 def gaussian_filter(X, sigma, **kwargs):
@@ -41,3 +45,38 @@ class Gaussian2DCartesian(Predictor):
             return filtered
 
         return np.where(mask, filtered)
+
+
+class Gaussian1DHealpix(Predictor):
+    cell_ids: _ArrayLike
+    grid_info: DGGSInfo
+
+    sigma: float
+    truncate: float = 4.0
+    kernel_size: int | None = None
+    weights_threshold: float | None = None
+
+    pad_kwargs: dict[str, Any] = field(
+        default_factory=lambda: {"mode": "constant", "constant_values": 0}
+    )
+
+    def __post_init__(self):
+        ring = hc.kernels.gaussian.compute_ring(
+            self.grid_info.resolution, self.sigma, self.truncate, self.kernel_size
+        )
+        self.padder = hc.padding.pad(
+            self.cell_ids, self.grid_info, ring=ring, **self.pad_kwargs
+        )
+        self.kernel = hc.kernels.gaussian_kernel(
+            self.cell_ids,
+            resolution=self.grid_info.resolution,
+            indexing_scheme=self.grid_info.indexing_scheme,
+            sigma=self.sigma,
+            truncate=self.truncate,
+            kernel_size=self.kernel_size,
+            weights_threshold=self.weights_threshold,
+        )
+
+    def predict(self, X, *, mask=None):
+        padded = self.padder.apply(X)
+        return hc.convolution.convolve(padded, self.kernel)
