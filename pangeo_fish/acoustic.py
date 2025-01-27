2448736ad4ea52bc76c5594554fe3f8d8c86e59a
Mazouni Quentin <37085377+QuentinMaz@users.noreply.github.com>
2025-01-27 15:49:28 +0100

    Warning into Exception in case of missing receivers (#123)
    Co-authored-by: Justus Magin <keewis@users.noreply.github.com>

--- pangeo_fish/acoustic.py
+++ pangeo_fish/acoustic.py
@@ -1,4 +1,3 @@
-import warnings
 import flox.xarray
 import numpy as np
 import pandas as pd
@@ -290,11 +289,7 @@ def emission_probability(
     maps_index = maps.indexes["deployment_id"]
     weights_index = weights.indexes["deployment_id"]
     if weights_index.difference(maps_index, sort=False).size > 0:
-        # print("WARNING: Some receiver ids `tag.acoustic` are not included in `tag.stations`.")
-        warnings.warn(
-            "Some receiver ids in `tag.acoustic` are not included in `tag.stations`.", 
-            UserWarning
-        )
+        raise ValueError("Some receiver ids in `tag.acoustic` are not included in `tag.stations`.")
 
     if nondetections == "ignore":
         fill_map = xr.ones_like(grid["cell_ids"], dtype=float).pipe(
