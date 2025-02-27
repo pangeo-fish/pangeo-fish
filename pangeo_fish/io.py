e0259e871d08f67864892b4e7e9b05178beadcb1
Justus Magin <keewis@posteo.de>
2025-02-27 11:46:20 +0100

    also remove the unused `read_detection_database`

--- pangeo_fish/io.py
+++ pangeo_fish/io.py
@@ -1,6 +1,4 @@
-import io
 import json
-import os
 import warnings
 
 import fsspec
@@ -27,41 +25,6 @@ def tz_convert(df, timezones):
     }
 
     return df.assign(**new_columns)
-
-
-def read_detection_database(url):
-    """read the detection database
-
-    Parameters
-    ----------
-    url : str, path-like or file-like
-        The file path, url, path-like or pre-opened file object of the file to open
-
-    Returns
-    -------
-    detection_database : pandas.DataFrame
-        The opened detection database
-
-    """
-    if isinstance(url, os.PathLike):
-        url = os.fsspath(url)
-
-    if isinstance(url, str):
-        opened = fsspec.open(url, mode="r")
-    else:
-        opened = url
-
-    with opened as f:
-        # work around the weird quoting
-        lines = (line.replace('"', "") for line in f)
-        data = "\n".join(lines)
-
-    content = io.StringIO(data)
-    return (
-        pd.read_csv(content, parse_dates=[1])
-        .rename(columns={"date_time": "time"})
-        .set_index("time")
-    )
 
 
 def open_tag(root, name, storage_options=None):
