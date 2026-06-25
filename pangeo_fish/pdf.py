6f3370fdfd471ca0aee3d4dac66e365e235cb29b
cchauvea <clement.chauveau@etu.ec-lyon.fr>
2026-06-25 11:14:24 +0200

    Use log into pdf
    To avoid multiplication, the part with multiplication is still available by uncommenting. No change in the structure

--- pangeo_fish/pdf.py
+++ pangeo_fish/pdf.py
@@ -5,7 +5,7 @@ import more_itertools
 import scipy.stats
 import xarray as xr
 from more_itertools import first
-
+import numpy as np
 from pangeo_fish.utils import _detect_spatial_dims, normalize
 
 
@@ -57,18 +57,32 @@ def combine_emission_pdf(raw, exclude=("initial", "final", "mask")):
     exclude = [n for n in more_itertools.always_iterable(exclude) if n in raw.variables]
 
     to_combine = [name for name in raw.data_vars if name not in exclude]
+    print(to_combine)
     if len(to_combine) == 1:
         pdf = raw[first(to_combine)].rename("pdf")
+    ### create bunch of nan and zero
+    # else:
+    #     pdf = (
+    #         raw[to_combine]
+    #         .to_array(dim="pdf")
+    #         .prod(dim="pdf", skipna=False)
+    #         .rename("pdf")
+    #     )
+    ### test with log
+            # Passer en log-space pour éviter l'underflow
     else:
-        pdf = (
-            raw[to_combine]
+        log_pdf = (
+            np.log(raw[to_combine])
             .to_array(dim="pdf")
-            .prod(dim="pdf", skipna=False)
-            .rename("pdf")
+            .sum(dim="pdf", skipna=False)  # somme en log = produit en linéaire
         )
-
+        # Stabilisation avant exp (soustraction du max par cellule)
+        log_pdf = log_pdf - log_pdf.max(dim="cells")  # remplace ... par ta dim spatiale
+        pdf = np.exp(log_pdf).rename("pdf")
+        
     if "final" in raw:
         pdf[{"time": -1}] = pdf[{"time": -1}] * raw["final"]
 
     spatial_dims = _detect_spatial_dims(raw)
+    print("haha")
     return xr.merge([raw[exclude], pdf.pipe(normalize, spatial_dims)])
